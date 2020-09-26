5a480626e94af63f93b5438b0bf861dfdc0c0cfc
Jimmy Kivlen <jimmykivlen@gmail.com>
2020-09-26 23:20:16 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,11 +60,12 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |seasons, holidays|
-    puts "#{seasons.capitalize}:"
-    holidays.each do |holidays, supplies|
-      puts" #{holidays.to_s.split('_').collect {|w| w.capitalize }.join(' ') }: #{supplies.join(", ")}"
-      end
+holiday_supplies.each do |season, holidays|
+    puts "#{season.capitalize}:"
+    holidays.each do |holiday, supplies|
+      puts"  #{holiday.to_s.split('_').map {|w| w.capitalize }.join(' ') }: #{supplies.join(", ")}"
     end
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
